b0e3cafbafe92e947d5e1c1bbe8002f03c7a66b2
Ryan Young <ryoung786@gmail.com>
2025-10-31 09:43:39 -0400

    update deps

--- mix.exs
+++ mix.exs
@@ -56,6 +56,7 @@ defmodule Dumper.MixProject do
       {:ecto_sql, "~> 3.5", only: [:dev, :test]},
       {:ecto_sqlite3, "~> 0.7", only: :test},
       {:floki, "~> 0.36.0", only: :test},
+      {:lazy_html, ">= 0.1.0", only: :test},
       {:faker, "~> 0.17", only: :test},
       {:styler, "~> 1.1", only: [:dev, :test], runtime: false}
     ]
